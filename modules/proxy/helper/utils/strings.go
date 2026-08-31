341b8439324045fee8835013593009053a5f30c6
Fathurrahman <fathur.ashter15@gmail.com>
2026-08-31 16:31:15 +0700

    Update: whitespaces

--- modules/proxy/helper/utils/strings.go
+++ modules/proxy/helper/utils/strings.go
@@ -1,71 +1,71 @@
 package utils
 
 import (
-    "net/http"
-    "net/url"
-    "strings"
+	"net/http"
+	"net/url"
+	"strings"
 
-    "github.com/nersus15/mini-proxy/mod-proxy/helper/types"
+	"github.com/nersus15/mini-proxy/mod-proxy/helper/types"
 )
 
 func InvalidURL(value string) bool {
-    return value == "" || value == "/"
+	return value == "" || value == "/"
 }
 
 func QueryParams(params map[string]string) string {
-    v := url.Values{}
-    for key, value := range params {
-        v.Add(key, value)
-    }
-    return v.Encode()
+	v := url.Values{}
+	for key, value := range params {
+		v.Add(key, value)
+	}
+	return v.Encode()
 }
 
 func ForwardPayload(headers map[string][]string, httpResponse *http.Response, url string, requestBody interface{}, raw any) types.BackupPayload {
-    statusText := http.StatusText(httpResponse.StatusCode)
-    if statusText == "" {
-        statusText = "OK"
-    }
+	statusText := http.StatusText(httpResponse.StatusCode)
+	if statusText == "" {
+		statusText = "OK"
+	}
 
-    expressRequestHeaders := make(map[string]string)
-    for key, values := range headers {
-        if len(values) > 0 {
-            expressRequestHeaders[strings.ToLower(key)] = strings.Join(values, ", ")
-        }
-    }
+	expressRequestHeaders := make(map[string]string)
+	for key, values := range headers {
+		if len(values) > 0 {
+			expressRequestHeaders[strings.ToLower(key)] = strings.Join(values, ", ")
+		}
+	}
 
-    expressResponseHeaders := make(map[string]string)
-    for key, values := range httpResponse.Header {
-        if len(values) > 0 {
-            expressResponseHeaders[strings.ToLower(key)] = strings.Join(values, ", ")
-        }
-    }
+	expressResponseHeaders := make(map[string]string)
+	for key, values := range httpResponse.Header {
+		if len(values) > 0 {
+			expressResponseHeaders[strings.ToLower(key)] = strings.Join(values, ", ")
+		}
+	}
 
-    cleanRequestPayload := types.AxiosRequestConfig{
-        URL:     url,
-        Method:  "POST",
-        Headers: expressRequestHeaders,
-        Data:    requestBody,
-    }
+	cleanRequestPayload := types.AxiosRequestConfig{
+		URL:     url,
+		Method:  "POST",
+		Headers: expressRequestHeaders,
+		Data:    requestBody,
+	}
 
-    payload := types.BackupPayload{
-        Request: cleanRequestPayload,
-        Response: types.AxiosResponse{
-            Data:       raw,
-            Status:     httpResponse.StatusCode,
-            StatusText: statusText,
-            Headers:    httpResponse.Header,
-            Config: types.AxiosRequestConfig{
-                URL:     url,
-                Method:  "POST",
-                Headers: expressRequestHeaders,
-                Data:    requestBody,
-            },
-        },
-    }
+	payload := types.BackupPayload{
+		Request: cleanRequestPayload,
+		Response: types.AxiosResponse{
+			Data:       raw,
+			Status:     httpResponse.StatusCode,
+			StatusText: statusText,
+			Headers:    httpResponse.Header,
+			Config: types.AxiosRequestConfig{
+				URL:     url,
+				Method:  "POST",
+				Headers: expressRequestHeaders,
+				Data:    requestBody,
+			},
+		},
+	}
 
-    if len(expressResponseHeaders) > 0 {
-        payload.Response.Headers = httpResponse.Header
-    }
+	if len(expressResponseHeaders) > 0 {
+		payload.Response.Headers = httpResponse.Header
+	}
 
-    return payload
+	return payload
 }
