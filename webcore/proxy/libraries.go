c4dc653f5e494dcb5f406fe7915bc3086cf63744
Fathurrahman <fathur.ashter15@gmail.com>
2026-08-31 16:32:43 +0700

    Updat: daftarkan redis di libraries - saat ini belum digunakan

--- webcore/proxy/libraries.go
+++ webcore/proxy/libraries.go
@@ -7,11 +7,13 @@ import (
 	// kafka "github.com/webcore-go/lib-kafka"
 	sentryLogger "github.com/nersus15/lib-go-sentry"
 	memory "github.com/webcore-go/lib-memory"
+	redis "github.com/webcore-go/lib-redis"
 	"github.com/webcore-go/webcore/app/core"
 )
 
 var APP_LIBRARIES = map[string]core.LibraryLoader{
 	"cache:memory":    &memory.MemoryLoader{},
+	"cache:redis":     &redis.RedisLoader{},
 	"database:sqlite": &sqlite.SqliteLoader{},
 	// "kafka:producer":  &kafka.KafkaProducerLoader{},
 	"cron":      &cron.CronLoader{},
